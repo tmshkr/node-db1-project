2ac872201ac2d09b56a8bf9d05b69152cd33eefd
Tim Shaker <tim@tmshkr.com>
2020-04-13 16:23:40 -0700

    add count

--- queries.sql
+++ queries.sql
@@ -27,7 +27,7 @@ UPDATE Customers
 WHERE CustomerName = 'Bilbo Baggins';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-SELECT DISTINCT City FROM Customers;
+SELECT COUNT(DISTINCT City) FROM Customers;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
 SELECT * FROM Suppliers
