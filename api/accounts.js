02a6a64994d4f9f12616d6c916f2fc63750864ad
Tim Shaker <tim@tmshkr.com>
2020-04-13 13:06:19 -0700

    GET /api/account/:id

--- api/accounts.js
+++ api/accounts.js
@@ -3,7 +3,7 @@ const db = require("../data/dbConfig.js");
 
 router.get("/", (req, res, next) => {
   db.select("*")
-    .from("account")
+    .from("accounts")
     .then((accounts) => {
       accounts.count > 0
         ? res.json(accounts)
@@ -12,6 +12,20 @@ router.get("/", (req, res, next) => {
     .catch((err) => {
       console.error(err);
       next({ code: 500, message: "There was an error getting the accounts" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db("accounts")
+    .where("id", req.params.id)
+    .then((accounts) => {
+      accounts.count > 0
+        ? res.json(accounts)
+        : res.status(404).send("account not found");
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("There was an error retrieving the account");
     });
 });
 
